42d85db2fe66b15656914894837de6ea3d96e56e
Michael Amaral <mike@meevi.dev>
2021-06-04 00:11:22 -0300

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -4,6 +4,6 @@ clear
 echo "starting app install script"
 
 # Brew installs
-brew install notion brave-browser vlc spotify dropbox rectangle transmission
+brew install notion brave-browser vlc spotify dozer rectangle transmission
 
 echo "common apps installed!"
