69726c95496daf2cd5f6727e66041e65067a1e95
Michael Amaral <mike@meevi.dev>
2022-08-15 00:51:48 -0300

    Update dev.sh

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -8,24 +8,18 @@ brew tap homebrew/cask-fonts
 brew tap AdoptOpenJDK/openjdk
 
 # Brew installs
-brew install node nvm yarn cocoapods watchman imagemagick fastlane figma sourcetree postman visual-studio-code android-studio reactotron font-fira-code flutter iterm2 zsh
+brew install node nvm yarn cocoapods watchman imagemagick fastlane figma fig sourcetree postman visual-studio-code android-studio reactotron font-fira-code flutter iterm2 zsh
 
 # XCode
 xcode-select --install
 xcode-select --switch /Applications/Xcode.app
 
-# Firebase CLI
-curl -sL firebase.tools | bash
 
 # yarn global packages
 yarn global add yalc
 
 #OhMyZsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-#Powerlevel10k
-brew install romkatv/powerlevel10k/powerlevel10k
-echo "source $(brew --prefix)/opt/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
 
 #Fix husky prehooks
 echo "export PATH="$(dirname $(which node)):$PATH"" > ~/.huskyrc
