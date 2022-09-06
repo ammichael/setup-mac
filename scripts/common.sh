044574555541c2e0380a26f7ac59c67dd78c5045
Michael Amaral <mike@meevi.dev>
2022-09-06 10:23:01 -0300

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -4,6 +4,6 @@ clear
 echo "starting app install script"
 
 # Brew installs
-brew install brave-browser rectangle istat-menus bartender cleanmymac
+brew install brave-browser rectangle istat-menus bartender cleanmymac numi
 
 echo "common apps installed!"
