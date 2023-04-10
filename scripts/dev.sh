f3e643ed93b88aab7c2dd9c899baaaac62ff9e68
Michael Amaral <mike@meevi.dev>
2023-04-10 01:18:57 -0300

    setup pyenv

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -10,6 +10,10 @@ brew tap AdoptOpenJDK/openjdk
 # Brew installs
 brew install node nvm yarn cocoapods watchman imagemagick pyenv fastlane figma fig sourcetree postman visual-studio-code android-studio reactotron font-fira-code flutter iterm2 zsh adoptopenjdk
 
+# Setup Pyenv
+pyenv install 3.11.3
+pyenv global 3.11.3
+
 # XCode
 xcode-select --install
 xcode-select --switch /Applications/Xcode.app
