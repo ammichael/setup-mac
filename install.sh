69b39dce75bc394d338a311201012b5893d87953
Michael Amaral <mike@meevi.dev>
2023-05-21 21:33:05 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
 
-# First Install Homebrew
-sh ./scripts/homebrew.sh
+echo "Selecione os scripts que você gostaria de rodar (digite os números separados por espaço):"
+echo "1. common.sh"
+echo "2. dev.sh"
+echo "3. homebrew.sh"
+read -p "Insira sua escolha: " options
 
-# Install Common Stuff
-sh ./scripts/common.sh
-
-# Install Dev Stuff
-sh ./scripts/common.sh
+for option in $options
+do
+    case $option in
+        1) sh common.sh;;
+        2) sh dev.sh;;
+        3) sh homebrew.sh;;
+        *) echo "Opção $option é inválida";;
+    esac
+done
