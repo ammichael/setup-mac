0ea026f83813f77a9c3884106772dde5212bcdea
Michael Amaral <michael.amaral@platformbuilders.io>
2023-05-21 22:58:33 -0300

    simplifying input for Shortcuts app

--- start.sh
+++ start.sh
@@ -1,15 +1,4 @@
 #! /bin/bash
 
-echo "Selecione os scripts que você gostaria de rodar (digite os números separados por espaço):"
-echo "1. Common - Utilidades gerais, navegador, etc."
-echo "2. Dev - Ferramentas de desenvolvimento"
-read -p "Insira sua escolha: " options
-
-for option in $options
-do
-    case $option in
-        1) sh common.sh;;
-        2) sh dev.sh;;
-        *) echo "Opção $option é inválida";;
-    esac
-done
+# Install Common Stuff
+sh ./scripts/common.sh ; sh ./scripts/common.sh
