55ba9c71648f589c03150e078a338b22825e824b
Michael Amaral <mike@meevi.dev>
2024-05-05 15:02:48 -0300

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -15,26 +15,21 @@ fi
 brew install arc \
  rectangle \
  notion \
+ notion-calendar \
  istat-menus \
  bartender \
  numi \
- ultimaker-cura
-# Curl installs
-curl -L -o cron-arm64.dmg https://download.cron.com/mac/dmg/arm64 && hdiutil attach cron-arm64.dmg && cp -r /Volumes/cron/Cron.app /Applications/ && hdiutil detach /Volumes/cron && rm cron-arm64.dmg
+ mas
 
 # App Store installs
 mas install 803453959 # Slack
 mas install 1147396723 # WhatsApp
-mas install 1176895641 # Spark
-mas install 1291898086 # Toggl Track
-mas install 1537056818 # Unzip
+mas install 6445813049 # Spark
 mas install 1528890965 # TextSniper
-mas install 747648890 # Telegram
 mas install 824183456 # Affinity Designer
 mas install 1339170533 # CleanMyMac X
 mas install 1402042596 # AdBlock for Safari
 mas install 1520333300 # Wappalyzer
-mas install 409789998 # Twitter
 
 # Local installs
 
@@ -42,10 +37,6 @@ mas install 409789998 # Twitter
 hdiutil attach ../apps/Tibia_Setup.dmg
 cp -r /Volumes/Tibia_Setup/Tibia.app /Applications/
 hdiutil detach /Volumes/Tibia_Setup
-# Aldente install
-hdiutil attach ../apps/AlDente-Pro.dmg
-cp -r /Volumes/AlDente/AlDente.app /Applications/
-hdiutil detach /Volumes/AlDente
 
 # Local copies
 cp -r ../apps/Chatterbox.app /Applications/ # Chatterbox
